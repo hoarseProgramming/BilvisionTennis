bc7af4c0f3230eae877d3a6df5d6563e42c40043
Hampus Eiderström Swahn <hampus.es@gmail.com>
2025-04-02 14:29:01 +0200

    Removed commented code

--- BilvisionTennis/TennisGame/Game.cs
+++ BilvisionTennis/TennisGame/Game.cs
@@ -4,16 +4,7 @@ namespace BilvisionTennis.TennisGame;
 
 public class Game
 {
-    //public Player PlayerOne { get; }
-    //public Player PlayerTwo { get; }
-    //public Umpire Umpire { get; }
     public string WrittenScore { get; set; } = $"{WrittenGamePoints[0]} | {WrittenGamePoints[0]}";
     public string VerbalScore { get; set; } = $"{VerbalGamePoint.Love} | {VerbalGamePoint.Love}";
     public bool HasWinner { get; set; }
-    //public Game(Player playerOne, Player playerTwo, Umpire umpire)
-    //{
-    //    PlayerOne = playerOne;
-    //    PlayerTwo = playerTwo;
-    //    Umpire = umpire;
-    //}
 }
--- BilvisionTennis/TennisGame/Set.cs
+++ BilvisionTennis/TennisGame/Set.cs
@@ -3,17 +3,7 @@ namespace BilvisionTennis.TennisGame
 {
     public class Set
     {
-        //public Player PlayerOne { get; }
-        //public Player PlayerTwo { get; }
-        //public Umpire Umpire { get; }
         public bool HasWinner { get; set; }
         public Game CurrentGame { get; set; }
-
-        //public Set(Player playerOne, Player playerTwo, Umpire umpire)
-        //{
-        //    PlayerOne = playerOne;
-        //    PlayerTwo = playerTwo;
-        //    Umpire = umpire;
-        //}
     }
 }
